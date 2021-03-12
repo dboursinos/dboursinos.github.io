f964ce190192b019ad4fa10bad4731f2f8ab839c
Dimitrios Boursinos <dimitris.boursinos@gmail.com>
2021-03-12 02:23:44 -0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4d67c6d35af7801a4381.js"
+    "url": "webpack-runtime-8d3caa2c30c6c954661d.js"
   },
   {
     "url": "framework-868c53fff4771b0bde98.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c5839d77cac3ba9bdfec655daa15e69a"
+    "revision": "11d62f0b8f924fd597efb7dfd5bf3b09"
   },
   {
     "url": "manifest.webmanifest",
